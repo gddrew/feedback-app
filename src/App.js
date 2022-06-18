edeb560c9da0e75a9d2c93fc6b6cea741f56f0fa
gddrew <gregoryddrew@gmail.com>
2022-06-17 21:31:18 -0500

    Finish 2-9 Intro to JSX redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello from the app component</h1>;
-}
+  // JSX
+  return (
+    <div className='container'>
+      <h1>My App</h1>
+      <p>Hello World!</p>
+    </div>
+  );
 
+  // Javascript - it can get messy, that's why we use JSX
+  // return React.createElement(
+  //   'div',
+  //   { className: 'container' },
+  //   React.createElement('h1', {}, 'My App'),
+  //   React.createElement('p', {}, 'Hello World!')
+  // );
+}
 export default App;
